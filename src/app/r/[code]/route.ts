11700ba65293b09728f15dd0b89c0eadfb651633
Maykon Sousa <maykon.sousa@hotmail.com>
2025-03-26 23:41:08 -0300

    feat: update params

--- src/app/r/[code]/route.ts
+++ src/app/r/[code]/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getTemplateData } from "@/lib/urlService";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { code: string } }
-) {
-  const { code } = params;
+type Params = Promise<{ code: string }>;
+
+export async function GET(request: NextRequest, { params }: { params: Params }) {
+  const { code } = await params;
+  
   const templateData = await getTemplateData(code);
 
   if (!templateData) {
