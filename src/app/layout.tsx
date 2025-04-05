b827ae325717d68cd2f432c92dace15d47f21c26
Maykon Sousa <maykon.sousa@hotmail.com>
2025-04-05 08:28:47 -0300

    feat: add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,10 @@ export default async function RootLayout({
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9591811085706785"
             crossOrigin="anonymous"
           ></script>
+          <meta
+            name="google-adsense-account"
+            content="ca-pub-9591811085706785"
+          ></meta>
         </head>
         <body>
           <PageContainer>
